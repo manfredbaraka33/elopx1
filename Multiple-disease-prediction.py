ab7edf328390c310753ac6bd57ecbcc95c7c4b24
manfredbaraka33 <163660029+manfredbaraka33@users.noreply.github.com>
2024-05-03 15:11:57 +0300

    Update Multiple-disease-prediction.py

--- Multiple-disease-prediction.py
+++ Multiple-disease-prediction.py
@@ -33,7 +33,7 @@ with st.sidebar:
 if selected == 'Diabetes prediction':
     # title 
     st.title('Diabetes prediction')
-    st.markdown('<style>div.block-container{padding-top:1rem}</style>',unsafe_allow_html=True)
+    st.markdown('<style>div.block-container{padding-top:2rem}</style>',unsafe_allow_html=True)
     #getting user input
     col1,col2,col3 = st.columns(3)
     
