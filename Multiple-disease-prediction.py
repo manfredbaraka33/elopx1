e94c257ee6b28cfdeb8f914a9b40a949929775c5
manfredbaraka33 <163660029+manfredbaraka33@users.noreply.github.com>
2024-05-27 12:30:24 +0300

    Update Multiple-disease-prediction.py

--- Multiple-disease-prediction.py
+++ Multiple-disease-prediction.py
@@ -19,12 +19,13 @@ parkinsons_model = pickle.load(open("parkinson.sav",'rb'))
 
 # the side bar for navigation
 
+
 # Custom CSS for styling the sidebar
 st.markdown("""
     <style>
         /* Change background color of selected sidebar section */
-        .element-container.sidebar .sidebar-content .block-container.stSelectbox div[data-baseweb="menu"] div:last-child {
-            background-color: #28a745; /* Change the color to Bootstrap success color */
+        .element-container.sidebar .sidebar-content .stSelectbox div[data-baseweb="menu"] div:last-child {
+            background-color: #28a745 !important; /* Change the color to Bootstrap success color */
         }
     </style>
 """, unsafe_allow_html=True)
@@ -32,7 +33,7 @@ st.markdown("""
 with st.sidebar:
     selected =option_menu('Elopyx-Medics',
                           [  'Home',
-                              'Diabetes prediction',
+                            'Diabetes prediction',
                            'Heart disease prediction',
                            'Parkinson`s disease pediction'
                            ],
